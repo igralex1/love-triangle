286a5deda467aa29e5e82bec7a0783326324a0e5
IGOR <igralex1@gmail.com>
2019-03-20 10:23:43 +0300

    complete on 89 %

--- src/index.js
+++ src/index.js
@@ -4,4 +4,25 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+
+  
+  var count = 0;
+  for (let i = 0 ; i < preferences.length; i++){
+    indexFirst = i;          
+    valueFirst = preferences[indexFirst];      
+  
+    indexSecond = valueFirst-1;                      
+    valueSecond = preferences[indexSecond];        
+  
+    indexThird = valueSecond -1;     
+    valueThird = preferences[indexThird];      
+  
+   
+    if (++indexFirst == valueThird){
+      count+=1;
+      
+    }
+  }
+  count = Math.round(count/3);
+  return count;
 };
